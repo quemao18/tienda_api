84464a2b0fde002bd93a1217e904bbc970f1b6b4
Alejandro@BEELZEBUB <alejandro@evotechnology.co>
2020-04-06 20:50:59 -0400

    style

--- application/controllers/api/Cart.php
+++ application/controllers/api/Cart.php
@@ -188,7 +188,31 @@ class Cart extends REST_Controller {
 					<head>
 					<link rel="stylesheet" type="text/css" href="http://rodasalias.com/tienda/app/libs/bootstrap/v336/css/bootstrap.min.css">
 					<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-					<title>'.htmlspecialchars($subject, ENT_QUOTES, $this->email->charset).'</title>			    
+					<title>'.htmlspecialchars($subject, ENT_QUOTES, $this->email->charset).'</title>
+					<style>
+					#customers {
+					  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+					  border-collapse: collapse;
+					  width: 80%;
+					}
+					
+					#customers td, #customers th {
+					  border: 1px solid #ddd;
+					  padding: 8px;
+					}
+					
+					#customers tr:nth-child(even){background-color: #f2f2f2;}
+					
+					#customers tr:hover {background-color: #ddd;}
+					
+					#customers th {
+					  padding-top: 12px;
+					  padding-bottom: 12px;
+					  text-align: left;
+					  background-color: #9e946b;
+					  color: white;
+					}
+					</style>			    
 				   </head>			    		
 					<body>
 					<a href="https://rodasalias.com/"><img src="https://rodasalias.com/tienda/img/logo.png" ></a>	
@@ -203,7 +227,7 @@ class Cart extends REST_Controller {
 					<br><br>Presupuesto solicitado:<br><br>';
 			 $body.= '
 					 
-	<table class="table table-striped">
+	<table class="table table-striped" id="customers">
 	<thead>
 			<tr>
 			 <th width="20%">Cantidad</th>
